67a354a057704652ab0a4f9ddb27fe1a156bad91
CzeanRey <czeanreymateo131@gmail.com>
2026-04-03 20:43:07 +0800

    Implement button click functionality
    Add event listeners for calculator buttons to handle input.

--- project/basic-calculator/script.js
+++ project/basic-calculator/script.js
@@ -1,1 +1,30 @@
+const button = document.querySelectorAll("button");
+const input = document.getElementById("result");
 
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", () => {
+    const buttondone = button[i].textContent;
+
+    if (buttondone === "C") {
+        clearinput();
+    }
+    else if(buttondone === "="){
+        calculate();
+    }
+    else{
+        appendvalue(buttondone);
+    }
+    });
+}
+
+function clearinput(){
+    input.value = "";
+}
+
+function calculate(){
+    input.value = eval(input.value);
+}
+
+function appendvalue(buttondone){
+    input.value += buttondone;
+}
