489f294654235c08b95fdc5ea0a6791601ed30a0
CzeanRey <czeanreymateo131@gmail.com>
2026-04-04 22:10:04 +0800

    Implement show/hide password functionality

--- project/show & hide password /script.js
+++ project/show & hide password /script.js
@@ -1,1 +1,13 @@
+const password = document.getElementById("password")
+const show = document.getElementById("show");
 
+show.addEventListener("click", () => {
+    if (password.type === "password") {
+        password.type = "text"
+        show.textContent = "Hide"
+    }
+    else{
+        password.type = "password"
+        show.textContent = "Show"
+    }
+})
