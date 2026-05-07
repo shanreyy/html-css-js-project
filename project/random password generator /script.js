ed0e618228c82dc697973e0fb97312fa53b98887
CzeanRey <czeanreymateo131@gmail.com>
2026-05-07 21:05:57 +0800

    Implement password generation on button click

--- project/random password generator /script.js
+++ project/random password generator /script.js
@@ -1,1 +1,16 @@
+const password = document.getElementById('password');
+const button = document.getElementById('generate');
 
+button.addEventListener('click', () => {
+
+    const passwordchar = "abbcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const maxlength = 20;
+    let generatedpassword = ""; 
+
+    for(let i = 0; i < maxlength; i++){
+        const randompassword = Math.floor(Math.random() * passwordchar.length);
+        const randomchar = passwordchar[randompassword]
+        generatedpassword += randomchar;
+    }
+    password.value = generatedpassword;
+});
