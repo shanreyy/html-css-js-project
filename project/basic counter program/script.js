38dbd5d92a034d8a15c6b3e99dcdf17e3761458f
CzeanRey <czeanreymateo131@gmail.com>
2026-05-07 21:28:05 +0800

    Implement basic counter functionality with buttons

--- project/basic counter program/script.js
+++ project/basic counter program/script.js
@@ -1,1 +1,21 @@
+const increment = document.getElementById("incrementBtn");
+const decrement = document.getElementById("decrementBtn");
+const reset = document.getElementById("resetBtn");
+const p = document.getElementById("p");
 
+let count = 0;
+
+increment.addEventListener("click", () => {
+    count++;
+    p.textContent = count;
+});
+
+decrement.addEventListener("click", () => {
+    count--;
+    p.textContent = count;
+});
+
+reset.addEventListener("click", () => {
+    count = 0;
+    p.textContent = count;
+});
